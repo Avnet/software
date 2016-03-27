4b0cc7eb265d5e26af51c418cd98885e27b23406
zedhed <kevin.keryk@avnet.com>
2016-03-26 18:28:13 -0700

    Added header for PicoZed cleanup script and tested against pz_petalinux_PZ7015_FMC2_20160311_232027 hdl tag.

--- petalinux/scripts/clean_pz_petalinux_bsp.sh
+++ petalinux/scripts/clean_pz_petalinux_bsp.sh
@@ -1,3 +1,50 @@
+# ----------------------------------------------------------------------------
+#       _____
+#      *     *
+#     *____   *____
+#    * *===*   *==*
+#   *___*===*___**  AVNET
+#        *======*
+#         *====*
+# ----------------------------------------------------------------------------
+# 
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+# 
+#  Please direct any questions to the PicoZed community support forum:
+#     http://www.picozed.org/forum
+# 
+#  Product information is available at:
+#     http://www.picozed.org/product/picozed
+# 
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2016 Avnet, Inc.
+#                              All rights reserved.
+# 
+# ----------------------------------------------------------------------------
+# 
+#  Create Date:         Mar 26, 2016
+#  Design Name:         PicoZed PetaLinux BSP Generator
+#  Module Name:         clean_pz_petalinux_bsp.tcl
+#  Project Name:        PicoZed PetaLinux BSP Generator
+#  Target Devices:      Xilinx Zynq-7000
+#  Hardware Boards:     PicoZed SOM
+# 
+#  Tool versions:       Xilinx Vivado 2015.2
+# 
+#  Description:         Clean Script for PicoZed PetaLinux BSP HW Platform
+# 
+#  Dependencies:        None
+#
+#  Revision:            Mar 26, 2016: 1.00 Initial version
+# 
+# ----------------------------------------------------------------------------
+
 #!/bin/bash
 
 # Set global variables here.
