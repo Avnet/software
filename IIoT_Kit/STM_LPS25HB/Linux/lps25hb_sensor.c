c19d84e948d7a5e55d1905e442a86e2110cae6db
Tom Curran <tom.curran@avnet.com>
2016-10-14 10:05:34 -0400

    Edits to source file headers for GPL v2 license.

--- IIoT_Kit/STM_LPS25HB/Linux/lps25hb_sensor.c
+++ IIoT_Kit/STM_LPS25HB/Linux/lps25hb_sensor.c
@@ -1,6 +1,6 @@
 
 /*
- * Modified for the Maxim 31855 SPI Sensor
+ * Modified for the ST Microelectronics LPS25HB Sensor
  * Copyright (c) 2016 Avnet, Inc.  All rights reserved.
  *
  * Avnet, Inc.
@@ -16,9 +16,19 @@
  * FROM CLAIMS OF INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY
  * AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  *
  */
  
